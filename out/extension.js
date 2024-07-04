ac1e6b8dfd3c55b2a5dba236a1fe957af00f304b
Dan Glorioso <125602370+danglorioso@users.noreply.github.com>
2024-07-04 03:55:15 -0400

    New feature to skip pre-existing headers

--- out/extension.js
+++ out/extension.js
@@ -44,18 +44,24 @@ function activate({ subscriptions }) {
     subscriptions.push(insertFunctionContract);
 }
 exports.activate = activate;
+// Command to insert a header template into the active text editor
 async function insertHeaderTemplate() {
+    // Initalize the active text editor
     const editor = vscode.window.activeTextEditor;
+    // If no active text editor open, handle the case and end command
     if (!editor) {
         await handleNoActiveEditor();
         return;
     }
+    // Prompt question of which files to insert header into
     const choice = await vscode.window.showQuickPick(['Just this file', 'All files in directory'], {
         placeHolder: 'Do you want to insert the header into just this file or every file in the directory?'
     });
+    // User cancelled the selection
     if (!choice) {
-        return; // User cancelled the selection
+        return;
     }
+    // Execute the choice from quick pick
     if (choice === 'Just this file') {
         console.log('Inserting header into single file.');
         insertHeaderIntoFile(editor.document.uri.fsPath);
@@ -71,9 +77,12 @@ async function handleNoActiveEditor() {
     const choice = await vscode.window.showQuickPick(['Yes', 'No'], {
         placeHolder: 'No active editor found. Do you want to insert headers into all files in the directory?'
     });
+    // User cancelled the selection or chose No
     if (!choice || choice === 'No') {
-        return; // User cancelled the selection or chose No
+        return;
     }
+    // User selected Yes, so insert headers into all files in the directory
+    /* TODO: MODIFY BELOW TO CALL THE INSERTHEADERINTODIRECTORY FUNCTION */
     const directoryPath = vscode.workspace.rootPath;
     if (directoryPath) {
         insertHeaderIntoDirectory(directoryPath);
@@ -82,47 +91,37 @@ async function handleNoActiveEditor() {
         vscode.window.showErrorMessage('No workspace directory found.');
     }
 }
-function insertHeaderIntoFile(filePath) {
-    const headerTemplate = `\
-/**************************************************************
- *
- *                ${path.basename(filePath)}
- *
- *     Assignment: 
- *         Author: 
- *           Date: ${new Date().toLocaleDateString()}
- *
- *     Summary: 
- * 
- **************************************************************/
-`;
-    vscode.workspace.openTextDocument(filePath).then((document) => {
-        vscode.window.showTextDocument(document).then((editor) => {
-            const position = new vscode.Position(0, 0);
-            editor.edit(editBuilder => {
-                editBuilder.insert(position, headerTemplate);
-            });
-        });
-    });
-}
+// async function insertHeaderIntoDirectory(directoryPath: string) {
+//     fs.readdir(directoryPath, async (err, files) => {
+//         if (err) {
+//             vscode.window.showErrorMessage('Failed to read directory: ' + err.message);
+//             return;
+//         }
+//         for (const file of files) {
+//             if (file.startsWith('.')) {
+//                 continue; // Skip files that begin with a dot
+//             }
+//             const filePath = path.join(directoryPath, file);
+//             if (fs.lstatSync(filePath).isFile() && !isBinaryFile(filePath)) {
+//                 // await insertHeaderIntoFileAsync(filePath);
+//                 await insertHeaderIntoFile(filePath);
+//             }
+//         }
+//     });
+// }
 async function insertHeaderIntoDirectory(directoryPath) {
-    fs.readdir(directoryPath, async (err, files) => {
-        if (err) {
-            vscode.window.showErrorMessage('Failed to read directory: ' + err.message);
-            return;
+    const files = fs.readdirSync(directoryPath);
+    for (const file of files) {
+        if (file.startsWith('.')) {
+            continue; // Skip files that begin with a dot
         }
-        for (const file of files) {
-            if (file.startsWith('.')) {
-                continue; // Skip files that begin with a dot
-            }
-            const filePath = path.join(directoryPath, file);
-            if (fs.lstatSync(filePath).isFile() && !isBinaryFile(filePath)) {
-                await insertHeaderIntoFileAsync(filePath);
-            }
+        const fullPath = path.join(directoryPath, file);
+        if (fs.lstatSync(fullPath).isFile() && !isBinaryFile(fullPath)) {
+            await insertHeaderIntoFile(fullPath); // Apply header to each file
         }
-    });
+    }
 }
-async function insertHeaderIntoFileAsync(filePath) {
+async function insertHeaderIntoFile(filePath) {
     const headerTemplate = `\
 /**************************************************************
  *
@@ -136,13 +135,64 @@ async function insertHeaderIntoFileAsync(filePath) {
  * 
  **************************************************************/
 `;
+    // Open the document and insert the header template
     const document = await vscode.workspace.openTextDocument(filePath);
-    const editor = await vscode.window.showTextDocument(document, { preview: false });
+    const firstLine = document.lineAt(0).text;
+    // Skip files that already have a header starting with /*
+    if (/^\/\*{2,}/.test(firstLine.trim())) {
+        console.log(`Skipping file with existing header: ${filePath}`);
+        return;
+    }
+    const editor = await vscode.window.showTextDocument(document);
     const position = new vscode.Position(0, 0);
     await editor.edit(editBuilder => {
         editBuilder.insert(position, headerTemplate);
     });
 }
+// function insertHeaderIntoFile(filePath: string) {
+//     const headerTemplate = `\
+// /**************************************************************
+//  *
+//  *                ${path.basename(filePath)}
+//  *
+//  *     Assignment: 
+//  *         Author: 
+//  *           Date: ${new Date().toLocaleDateString()}
+//  *
+//  *     Summary: 
+//  * 
+//  **************************************************************/
+// `;
+//     vscode.workspace.openTextDocument(filePath).then((document) => {
+//         vscode.window.showTextDocument(document).then((editor) => {
+//             const position = new vscode.Position(0, 0);
+//             editor.edit(editBuilder => {
+//                 editBuilder.insert(position, headerTemplate);
+//             });
+//         });
+//     });
+// }
+// async function insertHeaderIntoFileAsync(filePath: string) {
+//     const headerTemplate = `\
+// /**************************************************************
+//  *
+//  *                ${path.basename(filePath)}
+//  *
+//  *     Assignment: 
+//  *         Author: 
+//  *           Date: ${new Date().toLocaleDateString()}
+//  *
+//  *     Summary: 
+//  * 
+//  **************************************************************/
+// `;
+//     const document = await vscode.workspace.openTextDocument(filePath);
+//     const editor = await vscode.window.showTextDocument(document, { preview: false });
+//     const position = new vscode.Position(0, 0);
+//     await editor.edit(editBuilder => {
+//         editBuilder.insert(position, headerTemplate);
+//     });
+// }
 function insertFunctionContractTemplate() {
     const editor = vscode.window.activeTextEditor;
     if (editor) {
@@ -174,7 +224,7 @@ function insertFunctionContractTemplate() {
     }
 }
 function isBinaryFile(filePath) {
-    const binaryExtensions = ['.DS_Store', '.exe', '.bin', '.dll', '.so', '.dylib', '.pdf', '.png', '.jpg', '.jpeg', '.gif', '.bmp'];
+    const binaryExtensions = ['.DS_Store', '.exe', '.bin', '.dll', '.so', '.dylib', '.pdf', '.png', '.jpg', '.jpeg', '.gif', '.bmp', ".gitattributes", ".gitignore", ".gitmodules", ".gitkeep", ".git", ".gitconfig"];
     return binaryExtensions.some(extension => filePath.endsWith(extension));
 }
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -48,22 +48,28 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
     subscriptions.push(insertFunctionContract);
 }
 
+// Command to insert a header template into the active text editor
 async function insertHeaderTemplate() {
+    // Initalize the active text editor
     const editor = vscode.window.activeTextEditor;
 
+    // If no active text editor open, handle the case and end command
     if (!editor) {
         await handleNoActiveEditor();
         return;
     }
 
+    // Prompt question of which files to insert header into
     const choice = await vscode.window.showQuickPick(['Just this file', 'All files in directory'], {
         placeHolder: 'Do you want to insert the header into just this file or every file in the directory?'
     });
 
+    // User cancelled the selection
     if (!choice) {
-        return; // User cancelled the selection
+        return;
     }
 
+    // Execute the choice from quick pick
     if (choice === 'Just this file') {
         console.log('Inserting header into single file.');
         insertHeaderIntoFile(editor.document.uri.fsPath);
@@ -80,10 +86,14 @@ async function handleNoActiveEditor() {
         placeHolder: 'No active editor found. Do you want to insert headers into all files in the directory?'
     });
 
+    // User cancelled the selection or chose No
     if (!choice || choice === 'No') {
-        return; // User cancelled the selection or chose No
+        return; 
     }
 
+    // User selected Yes, so insert headers into all files in the directory
+
+    /* TODO: MODIFY BELOW TO CALL THE INSERTHEADERINTODIRECTORY FUNCTION */
     const directoryPath = vscode.workspace.rootPath;
     if (directoryPath) {
         insertHeaderIntoDirectory(directoryPath);
@@ -92,51 +102,40 @@ async function handleNoActiveEditor() {
     }
 }
 
-function insertHeaderIntoFile(filePath: string) {
-    const headerTemplate = `\
-/**************************************************************
- *
- *                ${path.basename(filePath)}
- *
- *     Assignment: 
- *         Author: 
- *           Date: ${new Date().toLocaleDateString()}
- *
- *     Summary: 
- * 
- **************************************************************/
-`;
+// async function insertHeaderIntoDirectory(directoryPath: string) {
+//     fs.readdir(directoryPath, async (err, files) => {
+//         if (err) {
+//             vscode.window.showErrorMessage('Failed to read directory: ' + err.message);
+//             return;
+//         }
 
-    vscode.workspace.openTextDocument(filePath).then((document) => {
-        vscode.window.showTextDocument(document).then((editor) => {
-            const position = new vscode.Position(0, 0);
-            editor.edit(editBuilder => {
-                editBuilder.insert(position, headerTemplate);
-            });
-        });
-    });
-}
+//         for (const file of files) {
+//             if (file.startsWith('.')) {
+//                 continue; // Skip files that begin with a dot
+//             }
+//             const filePath = path.join(directoryPath, file);
+//             if (fs.lstatSync(filePath).isFile() && !isBinaryFile(filePath)) {
+//                 // await insertHeaderIntoFileAsync(filePath);
+//                 await insertHeaderIntoFile(filePath);
+//             }
+//         }
+//     });
+// }
 
 async function insertHeaderIntoDirectory(directoryPath: string) {
-    fs.readdir(directoryPath, async (err, files) => {
-        if (err) {
-            vscode.window.showErrorMessage('Failed to read directory: ' + err.message);
-            return;
+    const files = fs.readdirSync(directoryPath);
+    for (const file of files) {
+        if (file.startsWith('.')) {
+            continue; // Skip files that begin with a dot
         }
-
-        for (const file of files) {
-            if (file.startsWith('.')) {
-                continue; // Skip files that begin with a dot
-            }
-            const filePath = path.join(directoryPath, file);
-            if (fs.lstatSync(filePath).isFile() && !isBinaryFile(filePath)) {
-                await insertHeaderIntoFileAsync(filePath);
-            }
+        const fullPath = path.join(directoryPath, file);
+        if (fs.lstatSync(fullPath).isFile() && !isBinaryFile(fullPath)) {
+            await insertHeaderIntoFile(fullPath); // Apply header to each file
         }
-    });
+    }
 }
 
-async function insertHeaderIntoFileAsync(filePath: string) {
+async function insertHeaderIntoFile(filePath: string) {
     const headerTemplate = `\
 /**************************************************************
  *
@@ -151,13 +150,70 @@ async function insertHeaderIntoFileAsync(filePath: string) {
  **************************************************************/
 `;
 
+    // Open the document and insert the header template
     const document = await vscode.workspace.openTextDocument(filePath);
-    const editor = await vscode.window.showTextDocument(document, { preview: false });
+    const firstLine = document.lineAt(0).text;
+
+    // Skip files that already have a header starting with /*
+    if (/^\/\*{2,}/.test(firstLine.trim())) {
+        console.log(`Skipping file with existing header: ${filePath}`);
+        return;
+    }
+
+    const editor = await vscode.window.showTextDocument(document);
     const position = new vscode.Position(0, 0);
     await editor.edit(editBuilder => {
         editBuilder.insert(position, headerTemplate);
     });
 }
+
+// function insertHeaderIntoFile(filePath: string) {
+//     const headerTemplate = `\
+// /**************************************************************
+//  *
+//  *                ${path.basename(filePath)}
+//  *
+//  *     Assignment: 
+//  *         Author: 
+//  *           Date: ${new Date().toLocaleDateString()}
+//  *
+//  *     Summary: 
+//  * 
+//  **************************************************************/
+// `;
+
+//     vscode.workspace.openTextDocument(filePath).then((document) => {
+//         vscode.window.showTextDocument(document).then((editor) => {
+//             const position = new vscode.Position(0, 0);
+//             editor.edit(editBuilder => {
+//                 editBuilder.insert(position, headerTemplate);
+//             });
+//         });
+//     });
+// }
+
+// async function insertHeaderIntoFileAsync(filePath: string) {
+//     const headerTemplate = `\
+// /**************************************************************
+//  *
+//  *                ${path.basename(filePath)}
+//  *
+//  *     Assignment: 
+//  *         Author: 
+//  *           Date: ${new Date().toLocaleDateString()}
+//  *
+//  *     Summary: 
+//  * 
+//  **************************************************************/
+// `;
+
+//     const document = await vscode.workspace.openTextDocument(filePath);
+//     const editor = await vscode.window.showTextDocument(document, { preview: false });
+//     const position = new vscode.Position(0, 0);
+//     await editor.edit(editBuilder => {
+//         editBuilder.insert(position, headerTemplate);
+//     });
+// }
 
 function insertFunctionContractTemplate() {
     const editor = vscode.window.activeTextEditor;
@@ -191,6 +247,6 @@ function insertFunctionContractTemplate() {
 }
 
 function isBinaryFile(filePath: string): boolean {
-    const binaryExtensions = ['.DS_Store', '.exe', '.bin', '.dll', '.so', '.dylib', '.pdf', '.png', '.jpg', '.jpeg', '.gif', '.bmp'];
+    const binaryExtensions = ['.DS_Store', '.exe', '.bin', '.dll', '.so', '.dylib', '.pdf', '.png', '.jpg', '.jpeg', '.gif', '.bmp',".gitattributes",".gitignore",".gitmodules",".gitkeep",".git",".gitconfig"];
     return binaryExtensions.some(extension => filePath.endsWith(extension));
 }
